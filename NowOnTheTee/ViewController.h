7c5316dbe694df1ea220de7b6fb159f5cf300031
Brian Rich <brian.d.rich@gmail.com>
2015-07-17 10:28:07 -0400

    Initial Commit

---
+++ NowOnTheTee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NowOnTheTee
+//
+//  Created by Brian Rich on 7/17/15.
+//  Copyright © 2015 Brian Rich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NowOnTheTee/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NowOnTheTee
+//
+//  Created by Brian Rich on 7/17/15.
+//  Copyright © 2015 Brian Rich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
